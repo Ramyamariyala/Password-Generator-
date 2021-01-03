8d29db2c678f98b11915aba19975ddfd42957911
Ramya.Mariyala <ramya.mariyala@gmail.com>
2021-01-03 18:04:48 -0500

    updated code

--- script.js
+++ script.js
@@ -13,20 +13,24 @@ var specialChar = ['!','@','#','$','%','^','&','*','(',')','_','-']
 // Write a function called generatePassword which will have series of prompts and user inputs
 function generatePassword() {
 
-  
+  do{
   var length = parseInt (prompt("How many characters would you like your password to contain?"));
     // WHEN prompted for the length of the password
   // THEN I choose a length of at least 8 characters and no more than 128 characters
-  if (length < 8 ||  length > 128) {
-  alert("Length must be between 8-128 characters")
-  return;
-  }
+  if (length < 8 ||  length > 128 || isNaN(length))
+   {
+    alert("Length must be between 8-128 characters") 
+    }
+  } while (length < 8 ||  length > 128 || isNaN(length))
+  //   var length = parseInt (prompt("How many characters would you like your password to contain?"));
+  // return;
+ 
 
 // THEN I am presented with a series of prompts for password criteria
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
 // we need show an error if they give us no characters to choose from
-  
+  do{
   var gotLower = confirm("Click Ok to confirm including lowercase characters.")
   var gotUpper = confirm("Click Ok to confirm including uppercase characters.")
   var gotSpecial = confirm("Click Ok to confirm including special characters.")
@@ -35,29 +39,37 @@ function generatePassword() {
 if (gotLower === false && gotUpper === false && gotSpecial === false && gotNumber === false){
  alert("You must select at least one character type!");
 }
+} while (gotLower === false && gotUpper === false && gotSpecial === false && gotNumber === false)
 
 var actualPassword = []
+var randomPassword = ""
 
 if (gotLower) {
   actualPassword = actualPassword.concat(lowercase)
+  randomPassword = randomPassword + lowercase[Math.floor(Math.random() * lowercase.length)];
 }
 if (gotUpper) {
   actualPassword = actualPassword.concat(uppercase)
+  randomPassword = randomPassword + uppercase[Math.floor(Math.random() * uppercase.length)];
 }
 if (gotSpecial) {
   actualPassword = actualPassword.concat(specialChar)
+  randomPassword = randomPassword + specialChar[Math.floor(Math.random() * specialChar.length)];
 }
 if (gotNumber) {
   actualPassword = actualPassword.concat(numeric)
+  randomPassword = randomPassword + numeric[Math.floor(Math.random() * numeric.length)];
 }
 
 //form all charc shows chosen, choose one randomly and add it to our password x number of times
 
-var randomPassword = ""
+var currentlength = randomPassword.length;
+console.log(currentlength) 
 
-for (var i = 0; i < length; i++) {
+for (var i = 0; i < length-(currentlength); i++) {
   randomPassword = randomPassword + actualPassword[Math.floor(Math.random() * actualPassword.length)];
   console.log(randomPassword)
+  
 }
 return randomPassword;
 
